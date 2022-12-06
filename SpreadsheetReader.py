97a8a7d66ad2bccf74662320bb4c9d1a874e2a90
FLawrence <1065311+FLawrence@users.noreply.github.com>
2022-12-06 16:49:31 +0000

    Update SpreadsheetReader.py
    Reads from spreadsheet and returns list of column headings

--- SpreadsheetReader.py
+++ SpreadsheetReader.py
@@ -1,1 +1,17 @@
 # Function to read data from spreadsheet
+
+import os
+from openpyxl import load_workbook
+
+def getSpreadsheet(filename):
+    path = os.path.join('data', filename) 
+    wb = load_workbook(filename = path)
+    
+    sheet = wb.worksheets[0]
+    column_list = [cell.value for cell in sheet[1]]
+    return column_list
+        
+        
+        
+
+print(getSpreadsheet('T 336_002.xlsx'))
