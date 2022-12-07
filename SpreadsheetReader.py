cfad50283b463a9d1fb9658a1fd7162ab2a12593
FLawrence <1065311+FLawrence@users.noreply.github.com>
2022-12-07 11:28:17 +0000

    Update SpreadsheetReader.py
    Copied test code into main file and added test for columns plus prevented None column names being added to the column list

--- SpreadsheetReader.py
+++ SpreadsheetReader.py
@@ -8,10 +8,15 @@ def getSpreadsheet(filename):
     wb = load_workbook(filename = path)
     
     sheet = wb.worksheets[0]
-    column_list = [cell.value for cell in sheet[1]]
+    column_list = [cell.value for cell in sheet[1] if cell.value is not None]
     return column_list
         
-        
+
+
+def test_loadfile(column_headings):
+    expected_columns = ['Letter','Series','Piece', 'Item', 'Treasury Case number', 'Home Office case number', 'First names/Initials', 'Surname', 'Age', 'Occupation', 'Award granted', 'Brief summary of grounds for recommendation'];
+    
+    assert column_headings == expected_columns;        
         
 
-print(getSpreadsheet('T 336_002.xlsx'))
+test_loadfile(getSpreadsheet('T 336_002.xlsx'))
